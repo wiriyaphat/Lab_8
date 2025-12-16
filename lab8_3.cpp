e226e37a1a4095ef032aa6f98996f802aab54ac7
Thiw <wiriyaphat6789@gmail.com>
2025-12-16 15:07:33 +0700

    Lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,37 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+int main(){
+    string Character;
+    long long Age, Height , Bounty;
+    cout << "Enter your age: ";
+    cin >> Age;
+    if (Age <= 25){
+        cout << "Enter your height: ";
+        cin >> Height;
+        if (Height < 100){
+            Character = "Chopper";
+        }
+        else if (Height < 180){
+            Character = "Usopp";
+        }
+        else {
+            cout << "Enter your bounty: ";
+            cin >> Bounty;
+            if (Bounty <= 1100000000){
+                Character = "Sanji";
+            }else {Character = "Zoro";}
+        }
+    }
+
+    else {
+        if(Age <= 60){
+            cout << "Enter your bounty: ";
+            cin >> Bounty;
+            if(Bounty > 500000000){
+                Character = "Jinbe";
+            }else {Character = "Franky";}
+        }else {Character = "Brook";}
+    }
+    cout << "Your character = " << Character;
+    return 0;
+}
